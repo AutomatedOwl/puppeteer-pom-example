55cc5205335396851c32079067696df97b646c04
Danny Simantov <danny@elminda.com>
2020-04-15 09:06:22 +0300

    Added chromium args as env var. Added const vars.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,47 @@
 import { WebHostPage } from "./pages/WebHostPage";
 const puppeteer = require("puppeteer-core");
 const findChromiumPath = require("./utils/find-chromium-path");
-require('dotenv').config();
+require("dotenv").config();
 
-const puppeteerExample = async () =>  {
+// Get chromium executable path
+const CHROMIUM_PATH =
+  findChromiumPath("./node_modules/puppeteer", "chrome-linux") + "/chrome";
+const BEFORE_CLOSE_DELAY_MILLISECS = 5000;
+const INPUT_TEXT = "Hello World";
 
-    // Get chromium executable path
-    const CHROMIUM_PATH =
-        (await findChromiumPath("./node_modules/puppeteer", "chrome-linux")) +
-        "/chrome";
+const puppeteerExample = async () => {
+  // Launch puppeteer
+  const browser = await puppeteer.launch({
+    executablePath: CHROMIUM_PATH,
+    headless: isHeadless(),
+    args: getChromiumArgs(),
+  });
 
-    // Launch puppeteer
-    const browser = await puppeteer.launch({
-        executablePath: CHROMIUM_PATH,
-        headless: process.env.HEADLESS==='true',
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--window-size=1920,1080",
-        ],
-    });
+  // Log Headless mode
+  console.log("Headless Mode: " + isHeadless());
 
-    // Log Headless mode
-    console.log('Headless Mode: ' + process.env.HEADLESS);
+  // Initialize WebHost page
+  let webHostPage = new WebHostPage(await browser.newPage());
 
-    // Initialize WebHost page
-    let webHostPage = new WebHostPage(await browser.newPage());
+  // Navigate to URL
+  await webHostPage.open();
 
-    // Navigate to URL
-    await webHostPage.open();
+  // Type 'Hello World' to text box
+  await webHostPage.typeToBox(INPUT_TEXT);
 
-    // Type 'Hello World' to text box
-    await webHostPage.typeToBox("Hello World");
+  // Wait before closing browser..
+  await webHostPage.delay(BEFORE_CLOSE_DELAY_MILLISECS);
 
-    // Wait before closing browser..
-    await webHostPage.delay(5000);
+  // Close browser;
+  await browser.close();
+};
 
-    // Close browser;
-    await browser.close();
+const getChromiumArgs = () => {
+  return process.env.CHROMIUM_ARGS.split(" ");
+};
+
+const isHeadless = () => {
+  return process.env.HEADLESS === "true";
 };
 
 puppeteerExample();
